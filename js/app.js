243a9dd2d3d0494b64645caa10c377ada9f71e34
chris <chrishayes042@gmail.com>
2023-01-08 15:14:16 -0700

    removed all the npm. Going back to vanilla

--- js/app.js
+++ js/app.js
@@ -1,12 +1,6 @@
-import { library, icon } from "@fortawesome/fontawesome-svg-core";
-import { faCat, faAmbulance } from "@fortawesome/free-solid-svg-icons";
-
 let iconTag = document.getElementById("icon");
-// iconTag.innerHTML = "I'm baffled";
+let aboutMeTag = document.getElementById("aboutMe");
 
-library.add(faCat, faAmbulance);
+iconTag.innerHTML = "<a href='https://github.com/chrishayes042'>CLICK HERE</a>";
 
-const iconContainer = document.createElement("span");
-iconContainer.innerHTML = icon({ prefix: "fas", iconName: "cat" }).html;
-
-iconTag.appendChild(iconContainer);
+aboutMeTag.innerHTML = "<p> I'm a software engineer</p>";
