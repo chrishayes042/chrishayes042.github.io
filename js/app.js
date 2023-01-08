01a8c15a27a6812a5fe442042df65d5a3fa3f8ef
chris <chrishayes042@gmail.com>
2023-01-08 11:04:55 -0700

    added fontawesome not sure if it works though

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 import { library, icon } from "@fortawesome/fontawesome-svg-core";
 import { faCat, faAmbulance } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faCat, faAmbulance);
+let iconTag = document.getElementById("icon");
+// iconTag.innerHTML = "I'm baffled";
 
-const iconTag = document.getElementById("icon");
+library.add(faCat, faAmbulance);
 
 const iconContainer = document.createElement("span");
 iconContainer.innerHTML = icon({ prefix: "fas", iconName: "cat" }).html;
 
-iconTag.innerHTML = <i class="fas fa-cat"></i>;
+iconTag.appendChild(iconContainer);
