f3f51d94605f40f665758e6755dc9313b854a105
chris <chrishayes042@gmail.com>
2023-01-13 09:49:31 -0700

    added some more p tags and added resume

--- js/app.js
+++ js/app.js
@@ -8,14 +8,23 @@ const App = () => {
                         <li><a href="index.html">Home</a></li>
                         <li><a href="">About</a></li>
                         <li><a href="">Contact</a></li>
-                     </ul>   
+                     </ul>
                     </nav>
                     <hr>
                     <div id="pContainer">
-                        <p></p>
+                        <p>Hello! I'm Chris and I'm a software engineer. I'm currently working full time as a developer. I have a bachelors degree in software development from WGU.</p>
                     </div>
                     <div id="imageDaisy">
                         <image id="daisy" src="assets/daisy.png"></image>
+                    </div>
+                    <div id="aboutMe">
+                    <h1>About Me</h1>
+                     <p>I'm  I love my cat Daisy</p> 
+                     <a href="assets/ChrisHayesResume2023.pdf">Resume</a>
+                    </div>
+                    <div id="contactDiv">
+                      <h1>Contact Me!</h1>
+                      <a href="https://github.com/chrishayes042">GitHub</a>
                     </div>
                     `;
   return div;
