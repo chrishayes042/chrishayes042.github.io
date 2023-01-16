76e870bf3fea8e176597d6b8f6f5255053f78b2e
chris <chrishayes042@gmail.com>
2023-01-15 22:21:10 -0700

    added webcam view. Cannot get it to stop though

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 const app = Vue.createApp();
 
+let All_mediaDevices = navigator.mediaDevices;
+// let video = document.getElementById("videoCam");
+// var vidStream;
+
 const root = Vue.createApp({
   data() {
     return {
@@ -12,6 +16,43 @@ const root = Vue.createApp({
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
+    },
+
+    openWebCam() {
+      if (!All_mediaDevices || !All_mediaDevices.getUserMedia) {
+        console.log("getUserMedia() not supported.");
+        return;
+      }
+      All_mediaDevices.getUserMedia({
+        audio: true,
+        video: true,
+      })
+        .then(function (vidStream) {
+          var video = document.getElementById("videoCam");
+          if ("srcObject" in video) {
+            video.srcObject = vidStream;
+          } else {
+            video.src = window.URL.createObjectURL(vidStream);
+          }
+          video.onloadedmetadata = function (e) {
+            video.play();
+          };
+        })
+        .catch(function (e) {
+          console.log(e.name + ": " + e.message);
+        });
+    },
+    // doesn't quite work...
+    closeWebCam() {
+      var video = document.getElementById("videoCam");
+      All_mediaDevices.getUserMedia({
+        audio: true,
+        video: true,
+      }).then(function () {
+        video.pause();
+        video.src = "";
+      });
+      console.log("Stream Stopped");
     },
   },
 });
